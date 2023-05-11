c16ecc9bf4803925ba4db7e70b6e662663b180b7
Michael Williams <Mwilliamsx720@gmail.com>
2023-05-10 23:42:15 -0400

    update

--- JavaScript_File.js
+++
@@ -1,6 +0,0 @@
-var Sent1 = ("This is the beginning of the string"); // Var Sent1 "assigned value within qoutes"//
-var Sent2 = ("and this is the end of the string"); // Var Sent2 "assigned value within qoutes"//
-
-document.write("Sent1" + "Sent2"); // Concatenateing the Sent1 & Sent2 variables together//
-
-window.alert(4046 - 2023); //Expression written to retain value of current year//
