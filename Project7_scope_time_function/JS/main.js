e232735237adb09013561f08e7cc29bd246ecffd
Michael Williams <Mwilliamsx720@gmail.com>
2023-05-15 19:53:12 -0400

    update commit

--- Project7_scope_time_function/JS/main.js
+++ Project7_scope_time_function/JS/main.js
@@ -1,4 +1,5 @@
-var my_Favorite_Num = 22;
+
+var my_Favorite_Num = 22; // Global - var assigned outside of function//
 function add_Nums_1() {
     document.write(20 + my_Favorite_Num + "<br>");
 }
@@ -8,9 +9,40 @@ function add_Nums_2() {
 add_Nums_1();
 add_Nums_2();
 
-function get_Date() {
-    if (new Date().getHours() < 18) {
-        document.getElementById("Greeting").innerHTML = "How are you today?";
+
+function add_Nums_1() {
+    var my_Favorite_Num = 22; // Local - var assigned outside of function//
+    document.write(20 + my_Favorite_Num + "<br>");
+}
+function add_Nums_2() {
+    document.write(my_Favorite_Num + 100 + "<br>");
+}
+add_Nums_1();
+add_Nums_2();
+
+function age_Function() {
+    age = document.getElementById("Age").value;
+    if (Age >= 18) {
+        Vote = "You are old enough to vote!";
     }
+    else {
+        Vote = "You are not old enough to vote!";
+    }
+    document.gegtElementById("how_Old_Are_You?").innerHTML = Vote;
+}
+
+function time_Function() {
+    var time = new Date() .getHours();
+    var reply;
+    if (time < 12 == time > 0) {
+        reply = "It is morning time!";
+    }
+    else if (time >= 12 == time < 18) {
+        reply = "It is afternoon.";
+    }
+    else {
+        reply = "It is evening time";
+    }
+    document.getElementById("time_Of_Day").innerHTML = reply;
 }
 
