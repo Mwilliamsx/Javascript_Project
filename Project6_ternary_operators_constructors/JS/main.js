67c16512188d0dd03ecdcc58df93a1ca5cbdfc0e
Michael Williams <Mwilliamsx720@gmail.com>
2023-05-15 14:35:48 -0400

    update

--- Project6_ternary_operators_constructors/JS/main.js
+++ Project6_ternary_operators_constructors/JS/main.js
@@ -1,6 +1,6 @@
 function age_Function() {
     var age, can_Vote;
     age = document.getElementById("age").value;
-    can_Vote = (age > 17) ? "You can " :"Your not old enough to ";
+    can_Vote = (age > 17) ? "You can " :"Your not old enough to "; // Age enter must be greater than 18 to vote//
     document.getElementById("voter").innerHTML = can_Vote + "\n vote";
 }
--- Project6_ternary_operators_constructors/JS/test.js
+++ Project6_ternary_operators_constructors/JS/test.js
@@ -4,9 +4,9 @@ function vehicle(Make, Model, Year, Color) {
     this.vehicle_Year = Year;
     this.vehicle_Color = Color;
 }
-var Jr = new vehicle("BMW", "3-Series", "2023", "Blue");
-var Carson = new vehicle("Dodge", "Challenger", "2023", "Red");
-var Skye = new vehicle("Mercedes-Benz", "C300", "2023", "Pink");
+var Jr = new vehicle("BMW", "3-Series", "2023", "Blue"); // Vehicle Make , Model, Year, Color//
+var Carson = new vehicle("Dodge", "Challenger", "2023", "Red"); // Vehicle Make , Model, Year, Color//
+var Skye = new vehicle("Mercedes-Benz", "C300", "2023", "Pink");// Vehicle Make , Model, Year, Color//
 function my_Function() {
     document.getElementById("keywords_And_Constructors").innerHTML =
     "Carson drives a " + Carson.vehicle_Color + "-colored" + Carson.vehicle_Model +
